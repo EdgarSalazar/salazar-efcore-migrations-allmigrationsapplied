df43b763436821dac87aed84edd0bcee6a3086ce
Edgar Froes <edgar.froes@gmail.com>
2018-07-17 17:36:27 -0300

    Initial commit

---
+++ Salazar.EFCore.Migrations.AllMigrationsApplied/DBContextExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Linq;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class DBContextExtension
+    {
+        public static bool AllMigrationsApplied(this DbContext context)
+        {
+            var applied = context.GetService<IHistoryRepository>()
+                .GetAppliedMigrations()
+                .Select(m => m.MigrationId);
+
+            var total = context.GetService<IMigrationsAssembly>()
+                .Migrations
+                .Select(m => m.Key);
+
+            return !total.Except(applied).Any();
+        }
+    }
+}
